3a21b424c8d5d72f924c8728299562c0a3e27456
oky31 <saputraoky6@gmail.com>
2018-02-02 13:16:52 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# membaca file cascade
+face_cascade = cv2.CascadeClassifier('cascadeClassifier/haarcascade_frontalface_default.xml')
+
+# membaca citra
+img = cv2.imread('muka3.jpg')
+
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray,1.3,5)
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
